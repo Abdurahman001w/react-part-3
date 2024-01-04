2b6fa3047bd050aaa8ffc99a54e49990e2821ae3
Abdurahman <atgerievrahman95@gmail.com>
2024-01-04 09:50:37 +0300

    react-part-3

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Message from "./components/message";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return <Message />;
+}
+export default App;
